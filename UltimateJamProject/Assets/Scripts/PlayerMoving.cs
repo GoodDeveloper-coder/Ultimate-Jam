cffed19893da8cfaf030abc3f184affafd8f716c
GoodDeveloper-coder <itmancik@gmail.com>
2023-08-30 15:53:11 +0300

    PlayereMoving

---
+++ UltimateJamProject/Assets/Scripts/PlayerMoving.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //------- Функция/метод, выполняемая каждый кадр в игре ---------
+    void Update()
+    {
+        Walk();
+    }
+
+    //------- Функция/метод для перемещения персонажа по горизонтали ---------
+    public float speed = 2f;
+    public Vector2 moveVector;
+
+    void Walk()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+}
